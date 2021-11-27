e01b5ea368d14b3e9c38088b26c979ad4e7f71ea
Joseph Zetka <josef.zetka@gmail.com>
2021-11-27 15:18:34 +0100

    s libuskou

---
+++ 23112021/Porovnani_nakladu_dvou_souboru_velicin.py
@@ -0,0 +1,82 @@
+from params_for_unit import params_for_unit
+from pdg_from_veliciny import pdg_from_veliciny, pdg_from_libuska
+from unit import Unit
+import pandas as pd
+import math
+import pdb
+
+bilancni_skupina = 1
+oh_ve_Veliciny = 22 # uved cele cislo napr pro 6.oh odpovida 5:00, proto zadam 5
+
+# FROM'Veliciny.................xlsx'
+#veliciny_cplex = pdg_from_libuska('libuska2_report.xlsx', oh_ve_Veliciny)
+breakpoint()
+veliciny_std = pdg_from_veliciny('6897472_211124_Veliciny PP.xlsx', oh_ve_Veliciny)
+breakpoint()
+veliciny_cplex = pdg_from_libuska('libuska2_report.xlsx', oh_ve_Veliciny)
+# .......................................................................................................................................................................
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# FROM KOTEL PP.LST
+##param = {'1st':'KOTEL_CostC', '2nd':'KOTEL_CostB', '3rd':'KOTEL_CostA', '4th':'KOTEL_Pdg', '5th': 'KOTEL_Pmaxtech', \
+##         '6th': 'KOTEL_Pmintech', '7th': 'KOTEL_CmaxDO', '8th':'KOTEL_BsSE'}
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+breakpoint()
+units_cplex = []
+print(f' ******* CPLEX **********')
+print(f' name      , costC        , pdg       , nkl ')
+for i in veliciny_cplex.index :   # Nacte vsechny indexy krome "b"
+       try:
+              if params_for_unit((i))[7] == bilancni_skupina:
+
+                  #breakpoint()
+                  u_cplex = Unit()
+                  u_cplex.name = str(i)
+                  u_cplex.costC = params_for_unit((i))[0]
+                  u_cplex.costB = params_for_unit((i))[1]
+                  u_cplex.costA = params_for_unit((i))[2]
+                  u_cplex.bsse = params_for_unit((i))[7]
+                  u_cplex.pdg = veliciny_cplex[i]
+                  u_cplex.vypocet_mer_nkl
+                  units_cplex.append(u_cplex)
+                  #print(f' name      , costC        , pdg       , nkl ')
+                  print(f'{u_cplex.name} {u_cplex.costC} {u_cplex.pdg} {u_cplex.mer_nkl}')
+       except:
+              pass
+
+units_std = []
+print(f' ******* STD **********')
+print(f' name      , costC        , pdg       , nkl ')
+for i in veliciny_std.index :   # Nacte vsechny indexy krome "b"
+       try:
+              if params_for_unit((i))[7] == bilancni_skupina:
+                  u_std = Unit()
+                  u_std.name = str(i)
+                  u_std.costC = params_for_unit((i))[0]
+                  u_std.costB = params_for_unit((i))[1]
+                  u_std.costA = params_for_unit((i))[2]
+                  u_std.bsse = params_for_unit((i))[7]
+                  u_std.pdg = veliciny_std[i]
+                  u_std.vypocet_mer_nkl
+                  units_std.append(u_std)
+                  print(f'{u_std.name} {u_std.costC} {u_std.pdg} {u_std.mer_nkl}')
+
+                  #print(f' name = {u_std.name}, costC= {u_std.costC}, pdg = {u_std.pdg}, nkl = {u_std.mer_nkl}')
+       except:
+              pass
+breakpoint()
+suma_nakladu_cplex = 0
+suma_pdg_cplex = 0
+for i in units_cplex:
+    suma_nakladu_cplex += i.mer_nkl
+    suma_pdg_cplex += i.pdg
+    
+print(f'suma_nakladu_cplex = {suma_nakladu_cplex}')
+print(f'suma_pdg_cplex = {suma_pdg_cplex}')
+    
+suma_nakladu_std = 0
+suma_pdg_std = 0
+for i in units_std:
+    suma_nakladu_std += i.mer_nkl
+    suma_pdg_std += i.pdg
+print(f'suma_nakladu_std = {suma_nakladu_std}')
+print(f'suma_pdg_std = {suma_pdg_std}')
---
+++ 23112021/pdg_from_veliciny.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import pdb
+def pdg_from_veliciny(nazev_souboru_veliciny, oh):
+       """ Returns Pdg or a type of shutdown for a specific bussiness interval"""
+       
+       veliciny = pd.read_excel(io = nazev_souboru_veliciny, sheet_name = 'Pdg',  skiprows = [0,1], \
+                                      usecols = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])
+       veliciny.rename(columns={'Unnamed: 1':'unit_name'}, inplace=True)
+       veliciny.rename(columns={'Obchodní hodina':'elna_name'}, inplace=True)
+       breakpoint()
+       for i in range (1, len(veliciny['elna_name'])):
+              print(f" i = {i}")
+              #breakpoint()
+              name = str(veliciny['elna_name'][i-1])
+              print(f" str(veliciny[elna_name][i]) = {str(veliciny['elna_name'][i])}" )
+              #print(f' type = {type(str(veliciny[elna_name][i]))}')
+              
+              if pd.isna(veliciny['elna_name'][i]) == True:
+                            print(f" isna ")
+                            veliciny['elna_name'][i]  = name
+              if str(veliciny['unit_name'][i]) == 'nan' or str(veliciny['unit_name'][i]) == 'NaN':
+                     veliciny['unit_name'][i] = 'n'
+       breakpoint()
+       veliciny.set_index(['elna_name', 'unit_name'], inplace = True)
+       b = [(              'CEZ', 'n'), (              'EDE', 'n'),(             'EME1', 'n'),(           'Celkem',   'n')] # indexy ktere nechci ze souboru Veliciny nacitat
+       #veliciny.index = list(filter(lambda el: el not in b, veliciny.index))
+       [veliciny.drop(labels = i,  inplace=True) for i in b]
+       breakpoint()
+       return veliciny.iloc[:,oh]
+
+def pdg_from_libuska(nazev_souboru_veliciny, oh):
+       """ Returns Pdg or a type of shutdown for a specific bussiness interval"""
+       breakpoint()
+       veliciny = pd.read_excel(io = nazev_souboru_veliciny, sheet_name = 'Pdg',  skiprows = [1], \
+                                      usecols = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26])
+       veliciny.rename(columns={'Unnamed: 2':'unit_name'}, inplace=True)
+       veliciny.rename(columns={'Unnamed: 1':'elna_name'}, inplace=True)
+       breakpoint()
+       for i in range (1, len(veliciny['elna_name'])):
+              print(f" i = {i}")
+              #breakpoint()
+              name = str(veliciny['elna_name'][i-1])
+              print(f" str(veliciny[elna_name][i]) = {str(veliciny['elna_name'][i])}" )
+              #print(f' type = {type(str(veliciny[elna_name][i]))}')
+              
+              if pd.isna(veliciny['elna_name'][i]) == True:
+                            print(f" isna ")
+                            veliciny['elna_name'][i]  = name
+              if str(veliciny['unit_name'][i]) == 'nan' or str(veliciny['unit_name'][i]) == 'NaN':
+                     veliciny['unit_name'][i] = 'n'
+       breakpoint()
+       veliciny.set_index(['elna_name', 'unit_name'], inplace = True)
+       #b = [(              'CEZ', 'n'), (              'EDE', 'n'),(             'EME1', 'n'),(           'Celkem',   'n')] # indexy ktere nechci ze souboru Veliciny nacitat
+       ##veliciny.index = list(filter(lambda el: el not in b, veliciny.index))
+       #[veliciny.drop(labels = i,  inplace=True) for i in b]
+       breakpoint()
+       return veliciny.iloc[:,oh]
